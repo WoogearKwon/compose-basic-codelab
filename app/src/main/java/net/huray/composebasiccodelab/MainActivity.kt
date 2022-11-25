60208acb22bdabfc91a7c71d829b8073e068d858
woogear.kwon <techkwon1717@hanmail.net>
2022-11-25 15:01:09 +0900

    Finishing touches

--- app/src/main/java/net/huray/composebasiccodelab/MainActivity.kt
+++ app/src/main/java/net/huray/composebasiccodelab/MainActivity.kt
@@ -7,14 +7,26 @@ import androidx.activity.compose.setContent
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,8 +68,7 @@ fun OnBoardingScreen(
     ) {
         Text("Welcome to the Basic Codelab!")
         Button(
-            modifier = Modifier.padding(vertical = 24.dp),
-            onClick = onContinueClicked
+            modifier = Modifier.padding(vertical = 24.dp), onClick = onContinueClicked
         ) {
             Text("Continue")
         }
@@ -74,8 +85,7 @@ fun OnBoardingPreview() {
 
 @Composable
 private fun Greetings(
-    modifier: Modifier = Modifier,
-    names: List<String> = List(1000) { "$it" }
+    modifier: Modifier = Modifier, names: List<String> = List(1000) { "$it" }
 ) {
     LazyColumn(modifier = modifier.padding(vertical = 4.dp)) {
         items(items = names) { name ->
@@ -86,13 +96,11 @@ private fun Greetings(
 
 @Composable
 private fun Greeting(name: String) {
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
     val extraPadding by animateDpAsState(
-        targetValue = if (expanded.value) 48.dp else 10.dp,
-        animationSpec = spring(
-            dampingRatio = Spring.DampingRatioMediumBouncy,
-            stiffness = Spring.StiffnessLow
+        targetValue = if (expanded) 48.dp else 10.dp, animationSpec = spring(
+            dampingRatio = Spring.DampingRatioMediumBouncy, stiffness = Spring.StiffnessLow
         )
     )
 
@@ -113,21 +121,29 @@ private fun Greeting(name: String) {
                         fontWeight = FontWeight.ExtraBold
                     )
                 )
+                if (expanded) {
+                    Text(
+                        text = ("Composem ipsum color sit lazy, " +
+                                "padding theme elit, sed do bouncy. ").repeat(4),
+                    )
+                }
             }
-            ElevatedButton(
-                onClick = { expanded.value = expanded.value.not() }
-            ) {
-                Text(text = if (expanded.value) "Show less" else "Show more")
+            IconButton(onClick = { expanded = expanded.not() }) {
+                Icon(
+                    imageVector = if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                    contentDescription = if (expanded) {
+                        stringResource(id = R.string.show_less)
+                    } else {
+                        stringResource(id = R.string.show_more)
+                    },
+                )
             }
         }
     }
 }
 
 @Preview(
-    showBackground = true,
-    widthDp = 320, heightDp = 320,
-    uiMode = UI_MODE_NIGHT_YES,
-    name = "Dark"
+    showBackground = true, widthDp = 320, heightDp = 320, uiMode = UI_MODE_NIGHT_YES, name = "Dark"
 )
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
