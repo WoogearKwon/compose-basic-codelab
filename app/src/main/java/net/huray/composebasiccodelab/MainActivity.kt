0ce6beea4c088e272fbbd6bdcde3958ba177f6b9
woogear.kwon <techkwon1717@hanmail.net>
2022-11-25 14:33:46 +0900

    Styling and theming your app

--- app/src/main/java/net/huray/composebasiccodelab/MainActivity.kt
+++ app/src/main/java/net/huray/composebasiccodelab/MainActivity.kt
@@ -1,5 +1,6 @@
 package net.huray.composebasiccodelab
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -14,6 +15,7 @@ import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import net.huray.composebasiccodelab.ui.theme.ComposeBasicCodelabTheme
@@ -105,7 +107,12 @@ private fun Greeting(name: String) {
                     .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello,")
-                Text(text = "$name!")
+                Text(
+                    text = "$name",
+                    style = MaterialTheme.typography.headlineLarge.copy(
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded.value = expanded.value.not() }
@@ -116,14 +123,12 @@ private fun Greeting(name: String) {
     }
 }
 
-@Preview
-@Composable
-fun MyAppPreview() {
-    ComposeBasicCodelabTheme {
-        MyApp(Modifier.fillMaxSize())
-    }
-}
-
+@Preview(
+    showBackground = true,
+    widthDp = 320, heightDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Dark"
+)
 @Preview(showBackground = true, widthDp = 320, heightDp = 320)
 @Composable
 fun GreetingsPreview() {
